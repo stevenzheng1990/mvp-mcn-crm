695491f15b5169036ee7b5cbe9d91d29ac47e045
stevenzheng1990 <64932178+stevenzheng1990@users.noreply.github.com>
2025-07-02 23:32:15 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -1,16 +1,5 @@
-@type {import('next').NextConfig}
+
 const nextConfig = {
-  env: {
-    GOOGLE_SHEETS_PRIVATE_KEY: process.env.GOOGLE_SHEETS_PRIVATE_KEY,
-    GOOGLE_SHEETS_CLIENT_EMAIL: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
-    GOOGLE_SHEET_ID: process.env.GOOGLE_SHEET_ID,
-  }
   reactStrictMode: true,
   swcMinify: true,
-  // 确保CSS正确处理
-  experimental: {
-    optimizeCss: false,  
-  }
 }
-module.exports = nextConfig
-
